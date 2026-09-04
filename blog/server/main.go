29150d22e4dac2261c46940103015a08d3a8e399
geekilx <iliakeshavarz23@gmail.com>
2026-09-04 14:53:29 +0330

    build(docker): add multi-stage build, compose stack, and graceful shutdown

--- blog/server/main.go
+++ blog/server/main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	blogv1 "github.com/geekilx/grpc-course/proto/blog/v1"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
-var collection *mongo.Collection
+var blogCollection *mongo.Collection
+var userCollection *mongo.Collection
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, falling back to system environment variables.")
+	}
 
 	caCert, err := os.ReadFile("grpc-cets/ca.pem")
 	if err != nil {
@@ -38,12 +49,27 @@ func main() {
 		ClientAuth:   tls.RequireAndVerifyClientCert,
 	}
 
-	client, err := mongo.Connect(options.Client().ApplyURI("mongodb://root:root@localhost:27017"))
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		mongoURI = "mongodb://root:root@localhost:27017"
+	}
+
+	client, err := mongo.Connect(options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatalf("failed to connect to mongodb: %v", err)
 	}
 
-	collection = client.Database("blogdb").Collection("blog")
+	blogCollection = client.Database("blogdb").Collection("blog")
+	userCollection = client.Database("blogdb").Collection("user")
+
+	indexModel := mongo.IndexModel{
+		Keys:    bson.D{{Key: "username", Value: 1}},
+		Options: options.Index().SetUnique(true),
+	}
+	_, err = userCollection.Indexes().CreateOne(context.Background(), indexModel)
+	if err != nil {
+		log.Fatalf("failed to create unique index: %v", err)
+	}
 
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
@@ -52,12 +78,31 @@ func main() {
 
 	log.Printf("server listening on port :50051")
 
-	s := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsConfig)))
-	blogService := CreateBlogService()
+	mongoRepo := NewMongoRepository(blogCollection)
+	userRepo := NewUserRepository(userCollection)
+
+	s := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsConfig)),
+		grpc.ChainUnaryInterceptor(AuthInterceptor, LogginInterceptor))
+	blogService := CreateBlogService(mongoRepo, userRepo)
 	blogv1.RegisterBlogServiceServer(s, blogService)
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	log.Println("shutting down server")
+	s.GracefulStop()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := client.Disconnect(ctx); err != nil {
+		log.Printf("error disconnecting mongo: %v", err)
 	}
 
 }
